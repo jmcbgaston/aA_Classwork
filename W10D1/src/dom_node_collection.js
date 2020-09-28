3e9938c8240f09f763942c9113dce37fc17169ba
Jose Gaston <jmcbgaston@gmail.com>
2020-09-28 16:29:07 -0700

    Add DOMNodeModules methods

--- W10D1/src/dom_node_collection.js
+++ W10D1/src/dom_node_collection.js
@@ -6,16 +6,40 @@ class DOMNodeCollection {
 
 DOMNodeCollection.prototype.html = function(stringArg) {
     stringArg = stringArg || null;
-    // debugger;
-    // if no argument, then return innerHTML msContentScript;
-    // if argument exists, then reassign innherHTML to new string;
 
-    // if no arg
     if (stringArg === null) {
         return this.htmlEleArray[0].innerHTML;
     } else {
         return this.htmlEleArray[0].innerHTML = stringArg;
     }
+}
+
+DOMNodeCollection.prototype.empty = function() {
+    this.htmlEleArray[0].innerHTML = "";
+    return this;
+}
+
+DOMNodeCollection.prototype.append = function(arg) {
+    // arg === $l() / HTMLElement / 'string'
+    // if (arg instanceof $l || arg instanceof HTMLElement || arg instanceof String) {
+    if (typeof arg === 'string' || arg === 'function' || arg instanceof HTMLElement) {
+        this.htmlEleArray.push(arg);
+        return this;
+    }
+}
+
+DOMNodeCollection.prototype.attr = function() {
+// sets a kv pair on html element
+// input.attr() => type : submit
+}
+
+DOMNodeCollection.prototype.addClass = function(arg) {
+    this.htmlEleArray[0].classList.add(arg)
+
+}
+
+DOMNodeCollection.prototype.removeClass = function(arg) {
+    this.htmlEleArray[0].classList.remove(arg)
 }
 
 module.exports = DOMNodeCollection;
